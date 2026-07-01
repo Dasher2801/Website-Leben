adc3ad3fdf764eb06d44085fa5ef36eaea5e7614
Philipp Buda <Philipp Buda>
2026-07-01 14:56:24 +0200

    add deploy workflow

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-const date = document.querySelector("#datepicker").value;
+
 document.querySelector("#app").innerHTML = "<p>loading...</p>";
+
+const date = document.querySelector("#datepicker").value;
+
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
-.then(data => {
-  document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
-    if (data.media_type === "image") {
-        document.querySelector("#app").innerHTML = `<img src="${data.url}"/>`;
-    } else {
-        document.querySelector("#app").innerHTML = `<video src="${data.url}" controls></video>`;
-    }
-    document.querySelector("#app").innerHTML = `<p>${data.explanation}</p>`;
-})
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
     let media;
 
     if (data.media_type === "image") {
-        media = `<img src="${data.url}"/>`;
+      media = `<img src="${data.url}"/>`;
     } else {
-        media = `<video src="${data.url}" controls></video>`;
+      media = `<video src="${data.url}" controls></video>`;
     }
-})
-document.querySelector("#app").innerHTML = `
-    <h1>${data.title}</h1>
-    ${media}
-    <p>${data.explanation}</p>
-`;
+
+    document.querySelector("#app").innerHTML = `
+      <h1>${data.title}</h1>
+      ${media}
+      <p>${data.explanation}</p>
+    `;
+  })
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+  });
